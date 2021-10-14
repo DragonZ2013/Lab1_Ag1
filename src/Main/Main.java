1256f686093f5f48e39d255994ad71f2acc03d9d
DragonZ <drag.popescu@yahoo.com>
2021-10-14 13:22:46 +0300

    Fixes + Comments

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -7,26 +7,26 @@ public class Main {
 
 
     public static void main(String[] args) {
-	// write your code here
         int[] grades = {10, 29, 30, 35, 38, 41, 42, 43, 45, 46, 36, 71, 77, 73, 81, 84};
+        System.out.println(aufgabe_4(grades));
         int[] print_1 = aufgabe_1(grades);
         for(int i: print_1)
             System.out.print(i+" ");
         System.out.println();
-        int[] print_2 = aufgabe_2(grades);
-        for(int i: print_2)
+        System.out.println(aufgabe_2(grades));
+        int[] print_3 = aufgabe_3(grades);
+        for(int i: print_3)
             System.out.print(i+" ");
         System.out.println();
-        System.out.println(aufgabe_3(grades));
         System.out.println(aufgabe_4(grades));
 
     }
 
     public static int[] aufgabe_1(int [] grades)
-    {
+    {//returns the array of insufficient grades
         int[] returnGrades={};
         for (int note: grades) {
-            if(note>=38) {
+            if(note<38) {
                 returnGrades = Arrays.copyOf(returnGrades, returnGrades.length + 1);
                 returnGrades[returnGrades.length - 1] = note;
             }
@@ -34,24 +34,16 @@ public class Main {
         return returnGrades;
     }
     public static int round(int x)
-    {
+    {//rounds the value if it's greater(or equal) than 38
         if(x<38)
             return x;
         if(x%5>=3)
             return x+5-x%5;
         return x-x%5;
     }
-    public static int[] aufgabe_2(int [] grades)
-    {
-        int[] returnGrades=grades;
-        for (int i=0;i<returnGrades.length;i++) {
-            returnGrades[i]=round(returnGrades[i]);
 
-        }
-        return returnGrades;
-    }
-    public static double aufgabe_3(int [] grades)
-    {
+    public static double aufgabe_2(int [] grades)
+    {//returns the average of the grades
         double sum=0;
         for (int note: grades) {
             sum+=note;
@@ -60,8 +52,18 @@ public class Main {
 
     }
 
+    public static int[] aufgabe_3(int [] grades)
+    {//returns an array with rounded grades
+        int[] returnGrades=new int[grades.length];
+        for (int i=0;i<grades.length;i++) {
+            returnGrades[i]=round(grades[i]);
+
+        }
+        return returnGrades;
+    }
+
     public static double aufgabe_4(int [] grades)
-    {
+    {//returns the maximum value of grades
         double max_val=0;
         for (int note: grades) {
             if(note>max_val)
